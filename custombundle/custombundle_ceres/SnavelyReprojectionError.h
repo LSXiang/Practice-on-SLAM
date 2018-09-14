1e1f37764133c5b5e7822d613ba6b7063d6d600b
Akatsuki <lsx499285437@163.com>
2018-09-14 18:06:39 +0800

    add bundle adjustment by ceres files

---
+++ custombundle/custombundle_ceres/SnavelyReprojectionError.h
@@ -0,0 +1,15 @@
+#ifndef __SNAVELYREPROJECTION_H__
+#define __SNAVELYREPROJECTION_H__
+
+#include <iostream>
+#include "ceres/ceres.h"
+
+#include "tools/rotation.h"
+#include "projection.h"
+
+class SnavelyReprojectionError
+{
+    
+};
+
+#endif /* __SNAVELYREPROJECTION_H__ */
