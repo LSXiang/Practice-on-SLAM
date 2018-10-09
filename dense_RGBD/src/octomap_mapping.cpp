ec14a50db4d233c69eb72fa7ad6659df6c7de609
Akatsuki <lsx499285437@163.com>
2018-10-09 14:57:06 +0800

    fix a bug & add octomap with the color

--- dense_RGBD/src/octomap_mapping.cpp
+++ dense_RGBD/src/octomap_mapping.cpp
@@ -9,6 +9,9 @@
 #include <boost/format.hpp>
 
 #include <octomap/octomap.h>
+#include <octomap/ColorOcTree.h>
+
+#define OCTOMAP_WITH_COLOR      1
 
 int main(int argc, char** argv)
 {
@@ -24,7 +27,7 @@ int main(int argc, char** argv)
     for (int i=0; i < 5; i ++) {
         boost::format fmt("../../data/%s/%d.%s");
         color_images.push_back(cv::imread((fmt%"color"%(i+1)%"png").str()));
-        depth_images.push_back(cv::imread((fmt%"depth"%(i+1)%"pgm").str()));
+        depth_images.push_back(cv::imread((fmt%"depth"%(i+1)%"pgm").str(), -1));
         
         double data[7] = {0};
         for (int i = 0; i < 7; i ++) {
@@ -45,7 +48,17 @@ int main(int argc, char** argv)
     std::cout << "The image is being converted to a OctoMap ..." << std::endl;
     
     // octomap tree
+#if OCTOMAP_WITH_COLOR
+    struct RGB_Color {
+        uchar r;
+        uchar g;
+        uchar b;
+    };
+    
+    octomap::ColorOcTree tree(0.05);
+#else
     octomap::OcTree tree(0.05);     // Resolution
+#endif
     
     for (int i = 0; i < 5; i++) {
         std::cout << "Image "<< i+1 << " is being converted ... " << std::endl;
@@ -55,6 +68,9 @@ int main(int argc, char** argv)
         Eigen::Isometry3d T = poses[i];
         
         octomap::Pointcloud cloud;  // the point cloud in octomap
+#if OCTOMAP_WITH_COLOR
+        std::vector<RGB_Color> rgb_colors;
+#endif
         
         for (int v = 0; v < color.rows; v++) {
             for (int u = 0; u < color.cols; u++) {
@@ -64,20 +80,43 @@ int main(int argc, char** argv)
                 
                 Eigen::Vector3d point;
                 point[2] = double(d) / depthScale;
-                point[1] = (u-cx)*point[2]/fx;
-                point[0] = (v-cy)*point[2]/fy;
+                point[0] = (u-cx)*point[2]/fx;
+                point[1] = (v-cy)*point[2]/fy;
                 Eigen::Vector3d point_world = T*point;
                 
                 cloud.push_back(point_world[0], point_world[1], point_world[2]);
+#if OCTOMAP_WITH_COLOR
+                RGB_Color rgb_color;
+                rgb_color.b = color.ptr<uchar>(v)[3*u];
+                rgb_color.g = color.ptr<uchar>(v)[3*u + 1];
+                rgb_color.r = color.ptr<uchar>(v)[3*u + 2];
+                rgb_colors.push_back(rgb_color);
+#endif
             }
         }
         
         tree.insertPointCloud(cloud, octomap::point3d(T(0,3), T(1,3), T(2,3)));
+        
+#if OCTOMAP_WITH_COLOR
+        int j = 0;
+//         for (octomap::point3d_collection::iterator p = cloud.begin(); p < cloud.end(); p ++) {
+        for (auto p = cloud.begin(); p < cloud.end(); p++) {
+            auto rgb = rgb_colors.data()[j];
+            tree.integrateNodeColor((*p).x(), (*p).y(), (*p).z(), rgb.r, rgb.g, rgb.b);
+            j ++;
+        }
+#endif
     }
     
     tree.updateInnerOccupancy();
     std::cout << "saving octomap..." << std::endl;
+
+#if OCTOMAP_WITH_COLOR
+    tree.write("octomap.ot");
+#else
     tree.writeBinary("octomap.bt");
+#endif
+
     return 0;
 }
 
